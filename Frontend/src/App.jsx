1012091300a94f9dc3ec480dfbf905147da4ca5e
Sambuddha Biswas <38996553+XiLucas@users.noreply.github.com>
2024-10-08 17:30:46 +0530

    Pushing rest of it

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -44,24 +44,33 @@ function App() {
                 amount={amount}
                 currencyOptions={options}
                 onCurrencyChange={(currency) => setAmount(amount)}
+                selectCurrency={from}
               />
             </div>
             <div className="relative w-full h-0.5">
               <button
                 type="button"
                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                onClick={swap}
               >
                 swap
               </button>
             </div>
             <div className="w-full mt-1 mb-4">
-              <InputBox label="To" />
+              <InputBox
+                label="To"
+                amount={convertedAmount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setTo(currency)}
+                selectCurrency={from}
+                amountDisable
+              />
             </div>
             <button
               type="submit"
               className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
             >
-              Convert
+              Convert{from.toUpperCase()} to {to.toUpperCase()}
             </button>
           </form>
         </div>
